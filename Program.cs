9739350011c39bf646dd8d20fba8f88e47849e52
Claudio Otte <cc.otte@hotmail.com>
2022-09-04 02:43:09 -0300

    documentation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using src.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,20 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Insurance Company API Dotnet",
+        Description = "Uma API idealizada para seguradoras ou empresas demandem por controle sobre contratos de seus clientes.",
+        Contact = new OpenApiContact
+        {
+            Name = "Contato com o Dev: Claudio Otte",
+            Url = new Uri("https://claudiootte.com")
+        },
+    });
+});
 
 var app = builder.Build();
 
